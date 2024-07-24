e120551011d772bbef94b9d333baa3673f589d98
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2024-07-24 15:52:17 +0300

    Deleting a reservation, another loading indicator using useTransition, updating aReservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateProfile(formData) {
   const session = await auth();
@@ -30,6 +32,74 @@ export async function updateProfile(formData) {
   }
 
   revalidatePath("/account/profile");
+}
+
+export async function updateReservation(formData) {
+  // .1 Authentication
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  // 2. Authorization
+  const guestBookings = await getBookings(session.user.guestId);
+
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  const bookingId = Number(formData.get("id"));
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking");
+
+  // 3) Building Update data
+  const updatedFields = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  // 4. Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updatedFields)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // 5) Error handling
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+
+  // 6) Revalidation
+  revalidatePath(`/accoutn/reservation/edit/${bookingId}`);
+
+  // 7) Redirecting
+  redirect("/account/reservations");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { data, error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
+
+  return data;
 }
 
 export async function signInAction() {
--- app/account/reservations/edit/[reservationId]/page.js
+++ app/account/reservations/edit/[reservationId]/page.js
@@ -1,7 +1,16 @@
-export default function Page() {
+import SubmitButton from "@/app/_components/SubmitButton";
+import { updateReservation } from "@/app/_lib/actions";
+import { getBooking, getCabin } from "@/app/_lib/data-service";
+
+export default async function Page({ params }) {
   // CHANGE
-  const reservationId = 23;
-  const maxCapacity = 23;
+  const reservationId = params.reservationId;
+
+  const reservation = await getBooking(reservationId);
+  const { numGuests, observations, cabinId } = reservation;
+  const cabin = await getCabin(cabinId);
+
+  const { maxCapacity } = cabin;
 
   return (
     <div>
@@ -9,12 +18,17 @@ export default function Page() {
         Edit Reservation #{reservationId}
       </h2>
 
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form
+        action={updateReservation}
+        className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+      >
         <div className="space-y-2">
+          <input defaultValue={reservationId} hidden={true} name="id" />
           <label htmlFor="numGuests">How many guests?</label>
           <select
             name="numGuests"
             id="numGuests"
+            defaultValue={numGuests}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
           >
@@ -34,15 +48,19 @@ export default function Page() {
             Anything we should know about your stay?
           </label>
           <textarea
+            defaultValue={observations}
             name="observations"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+          <SubmitButton pendingLabel="Updating...">
             Update reservation
-          </button>
+          </SubmitButton>
+          {/* <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+            Update reservation
+          </button> */}
         </div>
       </form>
     </div>
