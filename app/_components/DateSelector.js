7d56e8fd450873d7c1f89e99caf00e2dd4b4dad5
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2024-07-25 18:39:05 +0300

    Removing reservation immediately with the useOptimistic hook and finishing the date selector

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -20,10 +25,12 @@ function DateSelector({ bookedDates, settings, cabin }) {
   // CHANGE
   const { range, resetRange, setRange } = useReservation();
 
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+
+  const cabinPrice = numNights * (regularPrice - discount);
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -34,7 +41,7 @@ function DateSelector({ bookedDates, settings, cabin }) {
         className="pt-12 place-self-center"
         mode="range"
         onSelect={setRange}
-        selected={range}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -42,6 +49,11 @@ function DateSelector({ bookedDates, settings, cabin }) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(
+            curDate || bookedDates.some((date) => isSameDay(date, curDate))
+          )
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -77,6 +77,8 @@ export async function updateReservation(formData) {
 }
 
 export async function deleteReservation(bookingId) {
+  await new Promise((res) => setTimeout(res, 2000));
+  throw new Error();
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
